d510988f0db9502de68fc94faff6af31ad7cea22
Aryan-strack <aryanahmad478@gmail.com>
2026-06-27 22:40:55 +0500

    introduction

--- Introduction/helloworld.py
+++ Introduction/helloworld.py
@@ -1,3 +1,20 @@
 
 print("Hello World!")
 
+print(2 + 3) # addition
+print(5 - 2) # subtraction
+print(4 * 3) # multiplication
+print(6 / 2) # division
+print(5 % 2) # modulo
+print(5 // 2) # floor division
+print(2 ** 3) # exponentiation
+
+# checking Data types
+print(type(5)) # int
+print(type(5.0)) # float
+print(type("Hello")) # string
+print(type(True)) # boolean
+print(type([1, 2, 3])) # list
+print(type((1, 2, 3))) # tuple
+
+print(type({"name": "John", "age": 30})) # dictionary
