cb8c70281111f6ae9be0811ef8afc5b882ade497
Aryan-strack <aryanahmad478@gmail.com>
2026-07-04 22:00:39 +0500

    modify lists

---
+++ Lists/list.py
@@ -0,0 +1,35 @@
+empty_list = list()
+print(len(empty_list))  # 0
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot']
+
+animal_products = ['milk', 'meat', 'butter', 'yoghurt']
+
+web_tech = ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node', 'MongoDB']
+
+countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland']
+
+print("Fruits:", fruits)
+print('Number of fruits:', len(fruits))
+print("Vegetables:", vegetables)
+print('Number of vegetables:', len(vegetables))
+print("Animal Products:", animal_products)
+print('Number of animal products:', len(animal_products))
+print("Web Technologies:", web_tech)
+print('Number of web technologies:', len(web_tech))
+print("Countries:", countries)
+print('Number of countries:', len(countries))
+
+
+fruits = ['banana', 'orange', 'mango', 'lemon']
+first_fruit = fruits[0]
+print(first_fruit)  # banana
+second_fruit = fruits[1]
+print(second_fruit)  # orange
+last_fruit = fruits[3]
+print(last_fruit)  # lemon
+
+last_index = len(fruits) - 1
+last_fruit = fruits[last_index]
+
