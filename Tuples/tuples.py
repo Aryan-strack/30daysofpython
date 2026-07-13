246ba570a63bba79b9b2d467d5b198a9c603f144
Aryan-strack <aryanahmad478@gmail.com>
2026-07-13 14:52:02 +0500

    tuples

--- Tuples/tuples.py
+++ Tuples/tuples.py
@@ -28,3 +28,32 @@ fruits = ('banana', 'orange', 'mango', 'lemon')
 first_fruit = fruits[-4]
 second_fruit = fruits[-3]
 last_fruit = fruits[-1] 
+
+tpl = ('item1', 'item2', 'item3', 'item4', 'item5')
+all_items = tpl[0:5]
+all_items = tpl[0:]
+middle_items = tpl[1:4]
+
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+all_fruits = fruits[0:4]
+middle_fruits = fruits[1:3]
+last_fruits = fruits[2:4]
+
+
+tpl1 = ('item1', 'item2', 'item3')
+tpl2 = ('item4', 'item5', 'item6')
+tpl3 = tpl1 + tpl2
+
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+vegetables = ('tomato', 'potato', 'cabbage', 'onion', 'carrot')
+fruits_and_vegetables = fruits + vegetables
+
+
+
+tpl1 = ('item1' , 'item2', 'item3')
+del tpl1
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+del fruits
