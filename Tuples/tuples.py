6718cef3074f15b6b53fd928ffab2935946c3e37
Aryan-strack <aryanahmad478@gmail.com>
2026-07-13 14:17:16 +0500

    half tuples

---
+++ Tuples/tuples.py
@@ -0,0 +1,30 @@
+empty_tuple = ()
+
+empty_tuple = tuple()
+
+tpl = ('item1', 'item2', 'item3', 'item4', 'item5')
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+
+len(tpl)
+
+tpl = ('item1', 'item2', 'item3', 'item4', 'item5')
+first_item = tpl[0]
+second_item = tpl[1]
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[0]
+second_fruit = fruits[1]
+last_index = len(fruits) - 1
+last_fruit = fruits[last_index]
+
+
+tpl = ('item1', 'item2', 'item3', 'item4', 'item5')
+first_item = tpl[-5]
+second_item = tpl[-4]
+
+
+fruits = ('banana', 'orange', 'mango', 'lemon')
+first_fruit = fruits[-4]
+second_fruit = fruits[-3]
+last_fruit = fruits[-1] 
