572bb1c2a79209c1752d2cd0b1773ca40aa4a657
Aryan-strack <aryanahmad478@gmail.com>
2026-07-14 21:13:09 +0500

    lec dic start

---
+++ Dictionaries/dictionaries.py
@@ -0,0 +1,16 @@
+
+person = {
+    'first_name': 'John',
+    'last_name': 'Doe',
+    'age': 30,
+    'country': 'USA',
+    'is_married': False,
+    'skills': ['Python', 'JavaScript', 'SQL'],
+    'address': {
+        'street': '123 Main St',
+        'city': 'New York',
+        'state': 'NY',
+        }
+}
+
+print(person)
